621c48d5f82d8f7be3b343afb580cb98beeae1ee
DylanLiuH2O <913464928@qq.com>
2020-12-20 16:32:35 +0800

    增加UserDao测试类

---
+++ src/test/java/com/ocp/cuit/dao/TestUserDao.java
@@ -0,0 +1,21 @@
+package com.ocp.cuit.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestUserDao {
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testGetAllRetailers() {
+        System.out.println(userDao.getAllRetailers());
+    }
+
+    @Test
+    public void testGetAllOperators() {
+        System.out.println(userDao.getAllOperators());
+    }
+}
