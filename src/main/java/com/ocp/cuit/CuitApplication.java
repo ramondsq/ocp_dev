9f3fc4a78042fda3c95068244e5f64dda2b2f942
DylanLiuH2O <913464928@qq.com>
2020-12-20 16:29:21 +0800

    配置启动类

--- src/main/java/com/ocp/cuit/CuitApplication.java
+++ src/main/java/com/ocp/cuit/CuitApplication.java
@@ -1,9 +1,11 @@
 package com.ocp.cuit;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.ocp.cuit.dao")
 public class CuitApplication {
 
     public static void main(String[] args) {
