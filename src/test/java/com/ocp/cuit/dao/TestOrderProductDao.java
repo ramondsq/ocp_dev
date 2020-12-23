be385083d2e58695165be5ca11d04a4da11a8be5
dujie12345 <2107150050@qq.com>
2020-12-23 10:17:19 +0800

    进行了OrderProductDao的测试

--- src/test/java/com/ocp/cuit/dao/TestOrderProductDao.java
+++ src/test/java/com/ocp/cuit/dao/TestOrderProductDao.java
@@ -1,6 +1,7 @@
 package com.ocp.cuit.dao;
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
@@ -9,4 +10,25 @@ import javax.annotation.Resource;
 public class TestOrderProductDao {
     @Resource
     private OrderProductDao orderProductDao;
+
+    @Test
+    public void testGetAllStockOrderProducts() {
+        System.out.println(orderProductDao.getAllStockOrderProducts());
+    }
+
+    @Test
+    public void testGetStockOrderProducts() {
+        System.out.println(orderProductDao.getStockOrderProducts("202012210001"));
+    }
+
+    @Test
+    public void testGetAllWholesaleOrderProducts() {
+        System.out.println(orderProductDao.getAllWholesaleOrderProducts());
+    }
+
+    @Test
+    public void testGetWholesaleOrderProducts() {
+        System.out.println(orderProductDao.getWholesaleOrderProducts("202012210003"));
+    }
+
 }
