3f62626e8f7d0862e186454ec1f17ae52d12cb9e
Ramond Qu <quruis7@gmail.com>
2020-12-24 13:01:05 +0800

    写了 UserController 里的 retailerLogin 方法，新增了 service.impl 包，以及 UserServiceImpl 类

--- src/main/java/com/ocp/cuit/controller/UserController.java
+++ src/main/java/com/ocp/cuit/controller/UserController.java
@@ -2,22 +2,26 @@ package com.ocp.cuit.controller;
 
 import com.ocp.cuit.pojo.OperatorLogin;
 import com.ocp.cuit.pojo.RetailerLogin;
+import com.ocp.cuit.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class UserController {
-
+    @Resource
+    private UserService userService;
     /**
      * 经销商登录
      * @param retailerInfo
      * @return loginStatus: 0/1 失败/成功
      */
-    @RequestMapping("/retailerLogin")
+    @RequestMapping(path = "/retailerLogin", method = RequestMethod.POST)
     public Map<String, String> retailerLogin(RetailerLogin retailerInfo) {
-        return null;
+        return userService.retailerLogin(retailerInfo);
     }
 
     /**
---
+++ src/main/java/com/ocp/cuit/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.ocp.cuit.service.impl;
+
+import com.ocp.cuit.dao.UserDao;
+import com.ocp.cuit.pojo.OperatorLogin;
+import com.ocp.cuit.pojo.RetailerLogin;
+import com.ocp.cuit.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    UserDao userDao;
+
+    @Override
+    public Map<String, String> retailerLogin(RetailerLogin retailerLogin) {
+        RetailerLogin retailer = userDao.checkRetailerLogin(retailerLogin);
+
+        Map<String, String> map = new HashMap<>();
+
+        if (retailer != null) {
+            map.put("code", "1");
+            map.put("msg", "成功");
+        } else {
+            map.put("code", "0");
+            map.put("msg", "失败");
+        }
+
+        return map;
+    }
+
+    @Override
+    public Map<String, String> operatorLogin(OperatorLogin operatorLogin) {
+        return null;
+    }
+}
