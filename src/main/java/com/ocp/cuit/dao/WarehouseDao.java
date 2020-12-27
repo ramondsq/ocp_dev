5a9861e3291a2bd8acc6351087362725c15cd2ee
DylanLiuH2O <913464928@qq.com>
2020-12-27 14:29:10 +0800

    注释getAllWarehouses方法，修改getWarehouses方法为getAllWHNameNId

--- src/main/java/com/ocp/cuit/dao/WarehouseDao.java
+++ src/main/java/com/ocp/cuit/dao/WarehouseDao.java
@@ -11,15 +11,15 @@ import java.util.Map;
 @Mapper
 public interface WarehouseDao {
 
-    /**
-     * 获取所有仓库信息
-     * @return 仓库信息列表
-     */
-    List<Warehouse> getAllWarehouses();
+//    /**
+//     * 获取所有仓库信息
+//     * @return 仓库信息列表
+//     */
+//    List<Warehouse> getAllWarehouses();
 
     /**
      * 获取所有仓库的编号和名称
      * @return 所有仓库的编号和名称列表
      */
-    List<Map<String, Object>> getWarehouses();
+    List<Map<String, Object>> getAllWHNameNId();
 }
