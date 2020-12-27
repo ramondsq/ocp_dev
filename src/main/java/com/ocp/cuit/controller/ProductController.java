66029387ba06eb34df532b045d7eea79df95438d
DylanLiuH2O <913464928@qq.com>
2020-12-27 14:22:19 +0800

    完善getAllProducts方法，并修改返回值类型，修改请求映射

--- src/main/java/com/ocp/cuit/controller/ProductController.java
+++ src/main/java/com/ocp/cuit/controller/ProductController.java
@@ -1,18 +1,23 @@
 package com.ocp.cuit.controller;
 
+import com.ocp.cuit.service.ProductService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @RestController
 public class ProductController {
+    @Resource
+    private ProductService productService;
+
     /**
      * 获取所有产品信息
      * @return code状态码i(0 or 1), 所有产品的列表
      */
-    @RequestMapping("getAllProducts")
-    public Map<String, String> getAllProducts() {
-        return null;
+    @RequestMapping("/getAllProducts")
+    public Map<String, Object> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
