a196c90213a1a4e5a7270e5c7a4646181c4d6c5b
DylanLiuH2O <913464928@qq.com>
2020-12-27 14:15:20 +0800

    添加submitStockOrder方法

--- src/main/java/com/ocp/cuit/controller/OrderController.java
+++ src/main/java/com/ocp/cuit/controller/OrderController.java
@@ -1,10 +1,23 @@
 package com.ocp.cuit.controller;
 
+import com.ocp.cuit.pojo.StockOrder;
+import com.ocp.cuit.service.OrderService;
+import com.ocp.cuit.vo.SubmitStockOrderVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.Map;
+
 @RestController
 public class OrderController {
+    @Resource
+    private OrderService orderService;
 
+    @RequestMapping("/submitStockOrder")
+    public Map<String, Object> submitStockOrder(@RequestBody SubmitStockOrderVO submitStockOrderVO) {
+        return orderService.submitStockOrder(submitStockOrderVO);
+    }
 }
