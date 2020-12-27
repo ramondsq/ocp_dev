b3a08c385320c14b7729c1cc2fc1d288255038c3
DylanLiuH2O <913464928@qq.com>
2020-12-27 14:27:18 +0800

    添加@Service注解

--- src/main/java/com/ocp/cuit/service/UserService.java
+++ src/main/java/com/ocp/cuit/service/UserService.java
@@ -3,11 +3,9 @@ package com.ocp.cuit.service;
 import com.ocp.cuit.pojo.OperatorLogin;
 import com.ocp.cuit.pojo.Retailer;
 import com.ocp.cuit.pojo.RetailerLogin;
-import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-@Service
 public interface UserService {
     Map<String, String> retailerLogin(RetailerLogin retailerLogin);
     Map<String, String> operatorLogin(OperatorLogin operatorLogin);
