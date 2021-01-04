2f43cdbfcb8592673ccd39a3f72b95a8f59c739f
Dylan <913464928@qq.com>
2021-01-04 23:21:10 +0800

    增加submitWSOrder()方法、getOrders()方法

--- src/main/java/com/ocp/cuit/service/OrderService.java
+++ src/main/java/com/ocp/cuit/service/OrderService.java
@@ -1,9 +1,12 @@
 package com.ocp.cuit.service;
 
+import com.ocp.cuit.vo.RetailerGetAllOrdersVO;
 import com.ocp.cuit.vo.SubmitStockOrderVO;
 
 import java.util.Map;
 
 public interface OrderService {
     Map<String, Object> submitStockOrder(SubmitStockOrderVO submitStockOrderVO);
+    Map<String, Object> submitWSOrder();
+    Map<String, Object> getOrders(RetailerGetAllOrdersVO retailerGetAllOrdersVO);
 }
