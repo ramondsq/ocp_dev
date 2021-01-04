ec9289bfa4dc6d56912573fa3d68d88ff8a11325
Dylan <913464928@qq.com>
2021-01-04 22:35:19 +0800

    新增submitWSOrder()方法、getOrders()方法

--- src/main/java/com/ocp/cuit/controller/OrderController.java
+++ src/main/java/com/ocp/cuit/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ocp.cuit.controller;
 
 import com.ocp.cuit.pojo.StockOrder;
 import com.ocp.cuit.service.OrderService;
+import com.ocp.cuit.vo.RetailerGetAllOrdersVO;
 import com.ocp.cuit.vo.SubmitStockOrderVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,5 +22,15 @@ public class OrderController {
     @RequestMapping("/submitStockOrder")
     public Map<String, Object> submitStockOrder(@RequestBody SubmitStockOrderVO submitStockOrderVO) {
         return orderService.submitStockOrder(submitStockOrderVO);
+    }
+
+    @RequestMapping("/submitWSOrder")
+    public Map<String, Object> submitWSOrder() {
+        return null;
+    }
+
+    @RequestMapping("/getOrders")
+    public Map<String, Object> getOrders(RetailerGetAllOrdersVO retailerGetAllOrdersVO) {
+        return orderService.getOrders(retailerGetAllOrdersVO);
     }
 }
