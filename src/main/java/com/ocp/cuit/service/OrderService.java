60d65a816e624e894c1722c2d0ff4e500694bae0
Dylan <913464928@qq.com>
2021-01-06 18:47:17 +0800

    新增SubmitOrderReview()

--- src/main/java/com/ocp/cuit/service/OrderService.java
+++ src/main/java/com/ocp/cuit/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.ocp.cuit.service;
 
 import com.ocp.cuit.vo.RetailerGetAllOrdersVO;
+import com.ocp.cuit.vo.SubmitOrderReviewVO;
 import com.ocp.cuit.vo.SubmitStockOrderVO;
 import com.ocp.cuit.vo.SubmitWSOrderVO;
 
@@ -11,4 +12,5 @@ public interface OrderService {
     Map<String, Object> submitWSOrder(SubmitWSOrderVO submitWSOrderVO);
     Map<String, Object> getOrders(RetailerGetAllOrdersVO retailerGetAllOrdersVO);
     Map<String, Object> getRetailOrderRtr(String rtlog_user_name);
+    Map<String, Object> submitOrderReview(SubmitOrderReviewVO submitOrderReviewVO);
 }
