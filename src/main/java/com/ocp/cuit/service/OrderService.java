a728bfaeba749d3a1d676a38d567223b1df02999
Dylan <913464928@qq.com>
2021-01-06 16:15:46 +0800

    完善submitWSOrder()

--- src/main/java/com/ocp/cuit/service/OrderService.java
+++ src/main/java/com/ocp/cuit/service/OrderService.java
@@ -2,11 +2,12 @@ package com.ocp.cuit.service;
 
 import com.ocp.cuit.vo.RetailerGetAllOrdersVO;
 import com.ocp.cuit.vo.SubmitStockOrderVO;
+import com.ocp.cuit.vo.SubmitWSOrderVO;
 
 import java.util.Map;
 
 public interface OrderService {
     Map<String, Object> submitStockOrder(SubmitStockOrderVO submitStockOrderVO);
-    Map<String, Object> submitWSOrder();
+    Map<String, Object> submitWSOrder(SubmitWSOrderVO submitWSOrderVO);
     Map<String, Object> getOrders(RetailerGetAllOrdersVO retailerGetAllOrdersVO);
 }
